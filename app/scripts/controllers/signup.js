627998220e5e8321292c181e8e24656614181b09
singhmarut <singh.marut@gmail.com>
2014-07-14 14:54:24 +0530

    changes for login

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -15,7 +15,7 @@ angular.module('pupilsboardApp')
           password: $scope.user.password
         })
         .then( function() {
-         var myAlert = $alert({title: 'Holy guacamole!', content: 'Best check yo self, you\'re not looking too good.', placement: 'top', type: 'info', show: true});
+         var myAlert = $alert({title: 'Registration Complete', content: 'An email has been sent to your emailid.\n Please verify your email address', placement: 'top', type: 'info', show: true});
           // Account created, redirect to home
           $location.path('/');
         })
