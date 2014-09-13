3cc89ea4d8f41b62ce3b0cf9c78b212c4ff13de8
singhmarut <singh.marut@gmail.com>
2014-09-13 19:54:17 +0530

    Removed useless code

--- test/server/thing/api.js
+++ test/server/thing/api.js
@@ -3,37 +3,62 @@
 var should = require('should'),
     app = require('../../../server'),
     blog = require('../../../lib/controllers/blogController'),
+    analytics = require('../../../lib/controllers/analyticsController'),
     request = require('supertest');
 
-describe('GET /api/awesomeThings', function() {
-
-//    before(function(done) {
-//        user = new User({
-//            provider: 'local',
-//            name: 'Fake User',
-//            email: 'test@test.com',
-//            password: 'password'
-//        });
+//describe('GET /api/awesomeThings', function() {
 //
-//        // Clear users before testing
-//        User.remove().exec();
+////    before(function(done) {
+////        user = new User({
+////            provider: 'local',
+////            name: 'Fake User',
+////            email: 'test@test.com',
+////            password: 'password'
+////        });
+////
+////        // Clear users before testing
+////        User.remove().exec();
+////        done();
+////    });
+//
+//  it('should respond with JSON array', function(done) {
+//    request(app)
+//      .get('/api/awesomeThings')
+//      .expect(200)
+//      .expect('Content-Type', /json/)
+//      .end(function(err, res) {
+//        if (err) return done(err);
+//        res.body.should.be.instanceof(Array);
 //        done();
+//      });
+//  });
+//
+//
+//    it('should return 12', function(done) {
+//        blog.getCorrectDate().should.equal(12);
 //    });
+//
+//    it('should return questions', function(done) {
+//        var years = ['2008']
+//        analytics.getQuestionStats(years,'UPSC',['Polity'])
+//    });
+//
+//
+//});
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/awesomeThings')
+exports['calculate'] = function (test) {
+    test.equal( analytics.getQuestionStats(['2008'],'UPSC',['Polity']), 4);
+    test.done();
+};
+
+exports['addExtraTags'] = function (test) {
+     request(app)
+      .put('/api/question/tags')
       .expect(200)
-      .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
         done();
       });
-  });
 
-
-    it('should return 12', function(done) {
-        blog.getCorrectDate().should.equal(12);
-    });
-});
+};
